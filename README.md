7e55d2f9b8afc4bc20766340afbd9e4ca6e3beca
Keith <keitherhao@outlook.com>
2023-03-19 18:22:56 +0800

    Init.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # my_env_init
 自动安装常用工具
+
+# Usage
+./my_env_init.sh
---
+++ my_env_init.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+echo "Start exec $0"
+
+# mirror
+tmp="`cat /etc/pacman.d/mirrorlist | grep \"mirrors.ustc.edu.cn/archlinux\"`"
+echo $tmp
+if [ -z "$tmp" ];then
+	echo "Use ustc mirror."
+	sudo sed -i '1 i\Server = https://mirrors.ustc.edu.cn/archlinux/$repo/os/$arch' /etc/pacman.d/mirrorlist
+	sudo pacman -Sy
+	sudo pacman-key --init
+	sudo pacman-key --populate archlinux
+	#sudo pacman-key --refresh-keys
+fi
+
+# archlinuxcn
+tmp="`cat /etc/pacman.conf | grep \"archlinuxcn\"`"
+echo $tmp
+if [ -z "$tmp" ];then
+	echo "Add archlinuxcn mirror."
+	sudo sed -i '$a\[archlinuxcn]' /etc/pacman.conf
+	sudo sed -i '$a\Server = https://mirrors.ustc.edu.cn/archlinuxcn/$arch' /etc/pacman.conf
+	sudo pacman -Sy
+	sudo pacman -S archlinuxcn-keyring
+fi
+
+sudo pacman -Syyu
+sudo pacman -S vim git cmake curl zsh wget
+
+# Install oh-my-zsh
+if [ ! -d "$HOME/.oh-my-zsh" ];then
+	echo "Install oh-my-zsh"
+	wget https://gitee.com/mirrors/oh-my-zsh/raw/master/tools/install.sh
+	chmod +x install.sh
+	sed -i 's/REPO=${REPO:-ohmyzsh\/ohmyzsh}/REPO=${REPO:-mirrors\/oh-my-zsh}/' install.sh
+	sed -i 's/REMOTE=${REMOTE:-https:\/\/github.com\/${REPO}.git}/REMOTE=${REMOTE:-https:\/\/gitee.com\/${REPO}.git}/' install.sh
+	./install.sh
+	rm -rf install.sh*
+fi
+
+echo "End exec $0"
+
+exit 0
